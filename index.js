11136ea07ba09695d2efc12acb91ea074005b7d4
Ross Allen <ross@fivestar.local>
2024-12-29 16:09:01 -0800

    really frustrated...

--- index.js
+++ index.js
@@ -7,65 +7,63 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
 // * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/', async (req, res) => {
+    const map_territories = 'https://api.hubapi.com/crm/v3/objects/2-38694084?properties=status,territory_name,assigned_to';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(map_territories, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
+        console.log('Data:', data);
+        res.render('home', { title: 'Map Territories | Custom Object Data', data });      
+   } catch (error) {
         console.error(error);
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    try {
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
+   } catch (error) {
+        console.error(error);
+    }
+});
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const updates = {
+        properties: {
+            "territory_name": req.body.territory_name,
+            "status": req.body.status,
+            "assigned_to": req.body.assigned_to
+        }
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    const id = req.query.id;
+    const url = 'https://api.hubapi.com/crm/v3/objects/2-38694084';
+
+    const headers = {
+        Authorization: `Bearer ${}`,
+        'Content-Type': 'application/json'
     }
 
+    try { 
+        await axios.post(url, updates, { headers });
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+        res.status(500).send('Error updating or creating record');
+    }
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
